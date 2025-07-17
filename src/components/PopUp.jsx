c22d5cf6657b43fb9470cc7c062e8cea3bfd1d86
axle-blaze <gs33@iitbbs.ac.in>
2025-07-17 08:51:07 +0530

    Changes in the DB

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,3 +1,15 @@
+import React from "react";
+
 // SOme mnor changes
 
 
+const PopUp = ({ message, onClose }) => (
+    <div className="popup-overlay">
+        <div className="popup-content">
+            <span>{message}</span>
+            <button onClick={onClose}>Close</button>
+        </div>
+    </div>
+);
+
+export default PopUp;
